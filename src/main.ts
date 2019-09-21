fcf3e8957eb9a62fc97465b38788406cbf7a6c23
Ross Adamson <leopard.ross@gmail.com>
2019-09-20 22:57:45 -0600

    Enable support for a "default command" https://github.com/oclif/oclif/issues/277
    A command can now be designated as the default command (via
    package.json -> "oclif": {"defaultCommand": "<cmdId>"}).
    When a default command is specified, oclif will attempt to run
    that command when it doesn't recognize the command in the input,
    or when the input doesn't contain a command, or when
    there are no arguments passed to the CLI.
    
    This allows for a command to behave similarly to `yarn [run] <script>`,
    where `run` is the default command, such that running `yarn <script>`
    does the same thing as `yarn run <script>`.

--- src/main.ts
+++ src/main.ts
@@ -15,23 +15,37 @@ export class Main extends Command {
   }
 
   async run() {
+    if (this.argv.length === 0) return this._runDefaultCommand()
     let [id, ...argv] = this.argv
     this.parse({strict: false, '--': false, ...this.ctor as any})
     if (!this.config.findCommand(id)) {
       let topic = this.config.findTopic(id)
       if (topic) return this._help()
+      return this._runDefaultCommand()
     }
     await this.config.runCommand(id, argv)
   }
 
+  protected _getDefaultCommand(): string | undefined {
+    const oclif = this.config.pjson.oclif as any
+    return oclif.defaultCommand
+  }
+
+  protected async _runDefaultCommand() {
+    const defaultCommandId = this._getDefaultCommand()
+    if (defaultCommandId) {
+      await this.config.runCommand(defaultCommandId, [...this.argv])
+      return
+    } else return this._help()
+  }
+
   protected _helpOverride(): boolean {
-    if (['-v', '--version', 'version'].includes(this.argv[0])) return this._version() as any
-    if (['-h', 'help'].includes(this.argv[0])) return true
-    if (this.argv.length === 0) return true
-    for (let arg of this.argv) {
-      if (arg === '--help') return true
-      if (arg === '--') return false
+    if (this.argv.length === 0) {
+      if (this._getDefaultCommand()) return false
+      return true
     }
+    if (['-v', '-version', '--version', 'version'].includes(this.argv[0])) return this._version() as any
+    if (['-h', '-help', '--help', 'help'].includes(this.argv[0])) return true
     return false
   }
 
